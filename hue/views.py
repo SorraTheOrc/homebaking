e72224fd853bbcf169d0f7886358daa856661687
Ross Gardler <rgardler@apache.org>
2014-12-28 15:13:01 -0800

    Add Great Room controls and off buttons too

--- hue/views.py
+++ hue/views.py
@@ -5,16 +5,29 @@ username = 'homebakingrgardler'
 bridge = Bridge(device={'ip':'192.168.1.5'}, user={'name': username})
 
 def takeAction(request, group_id, action_id):
-    resource = {
-        'which':0,
-        'data':{
-            'action':{
-                'on':True,
-                'ct':166,
-                'bri':170
+    if action_id == 'on':
+        resource = {
+            'which':group_id,
+            'data':{
+                'action':{
+                    'on':True,
+                    'ct':166,
+                    'bri':160
+                    }
                 }
             }
-        }
+    elif action_id == 'off':
+        resource = {
+            'which':group_id,
+            'data':{
+                'action':{
+                    'on':False,
+                    }
+                }
+            }
+    else:
+        return render(request, 'homebaking/index.html', {'status': 'Unkown group action requested: ' + action_id})
+
     bridge.group.update(resource)
     return render(request, 'homebaking/index.html', {'status': 'Group action taken'})
 
